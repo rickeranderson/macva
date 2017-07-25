5615c8e20cd5ebf9eb3928313788184033a8a0a3
RickAnderson <rickanderson1@u.boisestate.edu>
2017-07-25 03:57:20 -0600

    Added comments

--- macva/src/app/components/characters/characters.component.ts
+++ macva/src/app/components/characters/characters.component.ts
@@ -1,202 +1,217 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../services/data.service';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../../services/data.service';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MdIconRegistry} from '@angular/material';
 
 @Component({
-  selector: 'app-characters',
-  templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.css']
+    selector: 'app-characters',
+    templateUrl: './characters.component.html',
+    styleUrls: ['./characters.component.css']
 })
 export class CharactersComponent implements OnInit {
 
-  characters:Character[];
-  tempCharacters:any[];
-  offset:number = 0;
-  limit:number = 20;
-  return:number = 0;
-  isLoading:boolean = true;
-  searchTerm:string = '';
-  tempSearchTerm:string = '';
-  detailEnabled:boolean = false;
-  orderBy:string = 'name';
+    characters: Character[];
+    tempCharacters: any[];
+    offset: number = 0;
+    limit: number = 20;
+    return: number = 0;
+    isLoading: boolean = true;
+    searchTerm: string = '';
+    tempSearchTerm: string = '';
+    detailEnabled: boolean = false;
+    orderBy: string = 'name';
 
-
-  showGetPrev(){
-    if(this.offset == 0){
-      return false;
+    /** Set search term and make request */
+    search() {
+        console.log(this.searchTerm);
+        this.selectedID = 0;
+        this.detailEnabled = false;
+        this.searchTerm = this.tempSearchTerm;
+        this.offset = 0;
+        this.getCharacters(this.offset);
     }
-    return true;
-  }
 
-  search(){
-    console.log(this.searchTerm);
-    this.selectedID = 0;
-    this.detailEnabled = false;
-    this.searchTerm = this.tempSearchTerm;
-    this.offset = 0;
-    this.getCharacters(this.offset);
-  }
-
-  showGetNext(){
-    if(this.return < this.limit){
-      return false;
-    }
-    return true;
-  }
-
-  getIsLoading(){
-    return this.isLoading;
-  }
-
-  isDetailEnabled(){
-    return this.detailEnabled;
-  }
-
-  getCharacters(offset) {
-    this.isLoading = true;
-    this.characters = [];
-    if(this.searchTerm.length == 0){
-      this.dataService.getCharactersByOffset(offset, this.limit, this.orderBy).subscribe((chars) => {
-        this.tempCharacters = chars.data.results;
-        console.log(chars.data);
-        this.characters = [];
-        this.return = chars.data.count
-        for (let i = 0; i < this.tempCharacters.length; i++) {
-          this.characters.push({
-            name: this.tempCharacters[i].name,
-            id: this.tempCharacters[i].id,
-            thumbnailPath: this.tempCharacters[i].thumbnail.path,
-            thumbnailExt: this.tempCharacters[i].thumbnail.extension
-          });
+    /** Hide prev if at begining */
+    showGetPrev() {
+        if (this.offset == 0) {
+            return false;
         }
-        this.isLoading = false;
-      });
-    } else {
-      this.dataService.getCharactersByOffsetSearch(offset, this.limit, this.searchTerm, this.orderBy).subscribe((chars) => {
-        this.tempCharacters = chars.data.results;
-        console.log(chars.data);
-        this.characters = [];
-        this.return = chars.data.count
-        for (let i = 0; i < this.tempCharacters.length; i++) {
-          this.characters.push({
-            name: this.tempCharacters[i].name,
-            id: this.tempCharacters[i].id,
-            thumbnailPath: this.tempCharacters[i].thumbnail.path,
-            thumbnailExt: this.tempCharacters[i].thumbnail.extension
-          });
+        return true;
+    }
+
+    /** Hide next if no more results */
+    showGetNext() {
+        if (this.return < this.limit) {
+            return false;
         }
-        this.isLoading = false;
-      });
+        return true;
     }
 
-  }
-
-  constructor(private dataService:DataService, iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-        'thumbs-up',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/icons/ic_view_comfy_black_48px.svg'));
-  }
-
-  ngOnInit() {
-   this.getCharacters(this.offset);
-  }
-
-  getNextCharacters(){
-    this.cleanSearch();
-    this.offset = this.offset + this.limit;
-    this.getCharacters(this.offset);
-  }
-
-  getPrevCharacters(){
-    this.cleanSearch();
-    this.offset = this.offset - this.limit;
-    this.getCharacters(this.offset);
-  }
-
-  cleanSearch(){
-    if(this.tempSearchTerm.length > 0 && this.searchTerm.length == 0){
-      this.tempSearchTerm= '';
+    /** To show or not to show spinny wheel thing*/
+    getIsLoading() {
+        return this.isLoading;
     }
-  }
 
-
-  //Character Detail Functions
-
-  selectedID:number = 0;
-  selectedCharacter:any;
-  numComics:number = 0;
-  numStories:number = 0;
-  numEvents:number = 0;
-  numSeries:number = 0;
-
-
-  selectCharacter(id:number){
-    this.isLoading = true;
-    this.selectedID = id;
-    this.getDetails();
-  }
-
-  getDetails(){
-    this.dataService.getCharacterDetails(this.selectedID).subscribe((chars) => {
-      this.selectedCharacter = chars.data.results[0];
-      if(this.selectedCharacter.description.length == 0){
-        this.selectedCharacter.description = 'No description available';
-      }
-      this.numComics = this.selectedCharacter.comics.items.length;
-      this.numStories = this.selectedCharacter.stories.items.length;
-      this.numEvents = this.selectedCharacter.events.items.length;
-      this.numSeries = this.selectedCharacter.series.items.length;
-
-      console.log(this.selectedCharacter);
-      this.isLoading = false;
-      this.detailEnabled = true;
-    });
-  }
-
-  checkComics(){
-    if(this.numComics == 0){
-      return true;
+    /** Checking to determine current view */
+    isDetailEnabled() {
+        return this.detailEnabled;
     }
-    return false;
-  }
 
-  checkStories(){
-    if(this.numStories == 0){
-      return true;
+    /** Make request to data service to retrieve data */
+    getCharacters(offset) {
+        this.isLoading = true;
+        this.characters = [];
+        if (this.searchTerm.length == 0) {
+            this.dataService.getCharactersByOffset(offset, this.limit, this.orderBy).subscribe((chars) => {
+                this.tempCharacters = chars.data.results;
+                console.log(chars.data);
+                this.characters = [];
+                this.return = chars.data.count
+                for (let i = 0; i < this.tempCharacters.length; i++) {
+                    this.characters.push({
+                        name: this.tempCharacters[i].name,
+                        id: this.tempCharacters[i].id,
+                        thumbnailPath: this.tempCharacters[i].thumbnail.path,
+                        thumbnailExt: this.tempCharacters[i].thumbnail.extension
+                    });
+                }
+                this.isLoading = false;
+            });
+        } else {
+            this.dataService.getCharactersByOffsetSearch(offset, this.limit, this.searchTerm, this.orderBy).subscribe((chars) => {
+                this.tempCharacters = chars.data.results;
+                console.log(chars.data);
+                this.characters = [];
+                this.return = chars.data.count
+                for (let i = 0; i < this.tempCharacters.length; i++) {
+                    this.characters.push({
+                        name: this.tempCharacters[i].name,
+                        id: this.tempCharacters[i].id,
+                        thumbnailPath: this.tempCharacters[i].thumbnail.path,
+                        thumbnailExt: this.tempCharacters[i].thumbnail.extension
+                    });
+                }
+                this.isLoading = false;
+            });
+        }
+
     }
-    return false;
-  }
 
-  checkEvents(){
-    if(this.numEvents == 0){
-      return true;
+    constructor(private dataService: DataService, iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
+        iconRegistry.addSvgIcon(
+            'thumbs-up',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/ic_view_comfy_black_48px.svg'));
     }
-    return false;
-  }
 
-  checkSeries(){
-    if(this.numSeries == 0){
-      return true;
+    /** Where first data is called */
+    ngOnInit() {
+        this.getCharacters(this.offset);
     }
-    return false;
-  }
 
-  goToMoreDetails(){
-    window.location.href = this.selectedCharacter.urls[1].url;
-  }
+    /** Go to next characters */
+    getNextCharacters() {
+        this.cleanSearch();
+        this.offset = this.offset + this.limit;
+        this.getCharacters(this.offset);
+    }
 
-  backToGrid(){
-    this.detailEnabled = false;
-  }
+    /** Go to previous characters */
+    getPrevCharacters() {
+        this.cleanSearch();
+        this.offset = this.offset - this.limit;
+        this.getCharacters(this.offset);
+    }
+
+    /** Remove search terms if not submitted */
+    cleanSearch() {
+        if (this.tempSearchTerm.length > 0 && this.searchTerm.length == 0) {
+            this.tempSearchTerm = '';
+        }
+    }
+
+    //////////////////////////////
+    //Character Detail Functions//
+    //////////////////////////////
+    selectedID: number = 0;
+    selectedCharacter: any;
+    numComics: number = 0;
+    numStories: number = 0;
+    numEvents: number = 0;
+    numSeries: number = 0;
+
+    /** Set id and grab details*/
+    selectCharacter(id: number) {
+        this.isLoading = true;
+        this.selectedID = id;
+        this.getDetails();
+    }
+
+    /** Calls data service to retrieve data from character with id */
+    getDetails() {
+        this.dataService.getCharacterDetails(this.selectedID).subscribe((chars) => {
+            this.selectedCharacter = chars.data.results[0];
+            if (this.selectedCharacter.description.length == 0) {
+                this.selectedCharacter.description = 'No description available';
+            }
+            this.numComics = this.selectedCharacter.comics.items.length;
+            this.numStories = this.selectedCharacter.stories.items.length;
+            this.numEvents = this.selectedCharacter.events.items.length;
+            this.numSeries = this.selectedCharacter.series.items.length;
+
+            console.log(this.selectedCharacter);
+            this.isLoading = false;
+            this.detailEnabled = true;
+        });
+    }
+
+    /** Check number of comics returned */
+    checkComics() {
+        if (this.numComics == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Check number of stories returned */
+    checkStories() {
+        if (this.numStories == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Check number of events returned */
+    checkEvents() {
+        if (this.numEvents == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Check number of series returned */
+    checkSeries() {
+        if (this.numSeries == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Navigate to character wiki */
+    goToMoreDetails() {
+        window.location.href = this.selectedCharacter.urls[1].url;
+    }
+
+    /** Change view back to grid */
+    backToGrid() {
+        this.detailEnabled = false;
+    }
 
 }
 
-
-
-interface Character{
-  name:string,
-  id:number,
-  thumbnailPath:string,
-  thumbnailExt:string
+/** Definition of character for grid view */
+interface Character {
+    name: string,
+    id: number,
+    thumbnailPath: string,
+    thumbnailExt: string
 }
--- macva/src/app/services/data.service.ts
+++ macva/src/app/services/data.service.ts
@@ -1,28 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  publicKey:string = '34f8c538accc8d5f931f49cad49a4d21';
+    publicKey: string = '34f8c538accc8d5f931f49cad49a4d21';
 
-  constructor(public http:Http) { }
+    constructor(public http: Http) {
+    }
 
-  getCharactersByOffset(offset:number, limit:number, order:string){
-    return this.http.get('https://gateway.marvel.com:443/v1/public/characters?orderBy='+order+'&limit='+limit+'&offset='+offset+'&apikey='+this.publicKey).map(res => res.json());
-  }
+    getCharactersByOffset(offset: number, limit: number, order: string) {
+        return this.http.get('https://gateway.marvel.com:443/v1/public/characters?orderBy=' + order + '&limit=' + limit + '&offset=' + offset + '&apikey=' + this.publicKey).map(res => res.json());
+    }
 
-  getCharactersByOffsetSearch(offset:number, limit:number, searchTerm:string, order:string){
-    return this.http.get('https://gateway.marvel.com:443/v1/public/characters?nameStartsWith='+searchTerm+'&orderBy='+order+'&limit='+limit+'&offset='+offset+'&apikey='+this.publicKey).map(res => res.json());
-  }
+    getCharactersByOffsetSearch(offset: number, limit: number, searchTerm: string, order: string) {
+        return this.http.get('https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=' + searchTerm + '&orderBy=' + order + '&limit=' + limit + '&offset=' + offset + '&apikey=' + this.publicKey).map(res => res.json());
+    }
 
-  getCharacterDetails(id:number){
-    return this.http.get('https://gateway.marvel.com:443/v1/public/characters/'+id+'?apikey='+this.publicKey).map(res => res.json());
-  }
+    getCharacterDetails(id: number) {
+        return this.http.get('https://gateway.marvel.com:443/v1/public/characters/' + id + '?apikey=' + this.publicKey).map(res => res.json());
+    }
 
-  getCover(uri:string){
-    return this.http.get(uri+'?apikey='+this.publicKey).map(res => res.json());
-  }
+    getCover(uri: string) {
+        return this.http.get(uri + '?apikey=' + this.publicKey).map(res => res.json());
+    }
 
 }
